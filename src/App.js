3539bbb7aab1403a72a03ba09130a677776517a3
Chris Rupper <chrisrupper@gmail.com>
2020-02-01 12:15:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let test_env = process.env.REACT_APP_TEST_VAR;
+console.log(process.env.REACT_APP_TEST_VAR);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <br/>
+          Test: {test_env}
         </p>
         <a
           className="App-link"
